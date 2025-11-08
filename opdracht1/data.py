40334f56fce52920f95be0f2e2ff62cb552b0bd4
DustinVII <dustinrr@gmail.com>
2025-11-08 12:01:57 -0300

    Functions en data appart gezet in functions.py en data.py

---
+++ opdracht1/data.py
@@ -0,0 +1,4 @@
+naam = "Dustin"
+leeftijd = 80
+hobbies = ["PC Gaming", "Coding", "Muziek", "Film & Photography", "Traveling"]
+tentamen_cijfers = { "Software Security & SE tools": 7.9, "Java & Databases": 8.0, "Algoritmen & Datastructuren": 10 }
---
+++ opdracht1/functions.py
@@ -0,0 +1,23 @@
+def assignment_nr(number):
+    print()
+    print()
+    print(f"\033[93m_____ ASSIGNMENT {number} __________________ \033[0m") #Hier is gebruik gemaakt van kleuren. \033[93m voor geel en \033[93m voor wit
+    print()
+
+def even_numbers():
+    for i in range(1, 11): #Dit is de loop. Het start vanaf 1 want anders komt 0 ook erbij, gezien 0 deelbaar is door 2. Eindigt bij 11, anders komt 10 niet erbij.
+        if i % 2 == 0: #Dit is de if condition. Het checkt of het getal deelbaar is door 2, want dat maakt het dan even
+            print(i)
+    
+def srd_prijs(koers, bedrag):
+    usd_koers = 39
+    eur_koers = 45
+
+    if koers == "USD":
+        print(f"{koers} 1 = SRD {usd_koers}")
+        print(f"{koers} {bedrag} = SRD {bedrag * usd_koers}")
+    elif koers == "EUR":
+        print(f"{koers} 1 = SRD {eur_koers}")
+        print(f"{koers} {bedrag} = SRD {bedrag * eur_koers}")
+
+    print()
