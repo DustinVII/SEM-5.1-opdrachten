ddcd0f17c348ddbef23576924405f534d702ab6c
DustinVII <dustinrr@gmail.com>
2025-11-09 08:05:30 -0300

    Split opdracht 1

---
+++ excercise1/main.py
@@ -0,0 +1,7 @@
+naam = "Dustin"
+hobbies = ["PC Gaming", "Coding", "Muziek", "Film & Photography", "Traveling"]
+
+if __name__ == "__main__":
+    print(f"Mijn naam is {naam} en mijn hobbies zijn:")
+    for hobby in hobbies:
+        print(f"- {hobby}")
